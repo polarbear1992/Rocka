5695f2a4c557f00305166bdbe30f5f64f8d1a28c
polarbear1992 <wqy919@yeah.net>
2019-11-29 17:55:23 +0800

    Add files via upload

--- rocka/shape_based_distance.py
+++ rocka/shape_based_distance.py
@@ -1,81 +1,78 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov 28 18:05:24 2019
+Created on Thu Nov 28 15:28:32 2019
 
 @author: PB
 """
 
-import numpy as np 
+import numpy as np
 
-__all__ = ['density_radius']
+__all__ = ['sbd_ele','SBD']
 
-
-def density_radius(sbd_arr,len_thresh,max_radius,slope_thresh,slope_diff_thresh):
+def sbd_ele(values1,values2):
     """
-    Given K-Nearest-Neighbor SBDs of each sample,calculate the density radius
-    for DESCAN clustering.
+    Given two time seires `values1' and `values2`,cross-correlation slides 
+    `values2' to `values1` to compute the inner-product for each shift `s`,the
+    range of shift `s` ∈ [-len(values2) + 1, len(values1)-1]
+    SBD is based of cross-correlation. SBD ranges from 0 to 2, where 0 means 
+    two time series have exactly the same shape. A smaller SBD means higher 
+    shape similarity.
     
     Args:
-        `sbd_arr`: np.ndarray, array of the K-Nearest-Neighbor SBD of each 
-            sample.
-        `len_thresh`: np.int32, the length of traget SBDs for candidate radius
-            search.
-        `max_radius`: np.float32, candidate radius are no larger than 
-            `max_radius`.
-        `slope_thresh`: np.float32, the slopes on the left and right of 
-            candidate point are no larger than `slope_thresh`
-        `slope_diff_thresh`: np.float32, the diff between leftslope and right-
-            slope of candidate point are no larger than `slope_diff_thresh`
-            
-    Returns:
-        np.float32: the final density radius is the largest value of all 
-            candidate radii.
-    """
-    src_index = np.argsort(sbd_arr)
-    sbd_arr_sorted = sbd_arr[src_index][::-1]
-    candidates_index = np.argwhere(sbd_arr_sorted<=max_radius)
-    start = np.min(candidates_index)
-    end = len(sbd_arr_sorted)
-    
-    def find_candidate_radius(sbd_arr_sorted,start,end,candidates):
-        """
-        Given reverse sorted K-Nearest-Neighbor SBDs of each sample,calculate the density 
-        radius for DESCAN clustering.
-        A divide and conquer strategy is used for candidate radius finding.
+        values1(np.ndarray): time series 1
+        values2(np.ndarray): time series 2
         
-        Args:
-            `sbd_arr_sorted`: np.ndarray, reverse sorted array of the K-Nearest
-                -Neighbor SBD of each sample.
-            `start`: np.int32, the begain index of target SBDs.
-            `end`: np.int32, the end index of target SBDs.
-            `candidates`: np.ndarray, the indexes of all candidate radii.
-            
-        Returns:
-            `candidates`: np.ndarray, the indexes of all candidate radii.
-        """
-        if end - start <= len_thresh:
-            return
-        radius,diff = -1,2
-        for i in range(start+1,end):
-            leftslope = (sbd_arr_sorted[i]-sbd_arr_sorted[start])/(i-start)
-            rightslope = (sbd_arr_sorted[end-1]-sbd_arr_sorted[i])/(end-1-i)
-            
-            if leftslope > slope_thresh or rightslope > slope_thresh:
-                continue
-            if np.abs(leftslope - rightslope) < diff:
-                diff = leftslope - rightslope
-                radius = i
-        if diff < slope_diff_thresh:
-            np.append(candidates,radius)
-        find_candidate_radius(sbd_arr_sorted,start,radius,candidates)
-        find_candidate_radius(sbd_arr_sorted,radius+1,end,candidates)
+    Returns:
+        np.float32: the SBD between `values1` and `values2`
+    """
+    #get the 2 norm
+    l2_values1 = np.linalg.norm(values1)
+    l2_values2 = np.linalg.norm(values2)
+    #get the cross-correlation of each shift `s`
+    cross_corre = np.convolve(values1,values2,mode = 'full')
     
-    candidate = np.empty((0),np.int32)
-    candidates = find_candidate_radius(sbd_arr_sorted,start,end,candidate)
-    print(candidates)
-    if candidates is not None:
-        radius_candidates = np.max(sbd_arr_sorted[candidates])
-        return radius_candidates
-    else:
-        raise ValueError('There is no qualified density raidus.')
+    #return the SBD between `values1` and `values2`
+    return 1 - np.max(cross_corre)/(l2_values1 * l2_values2)
+
+def SBD(values_list,minPts = 4):
+    """
+    Caculate the shape based distance(SBD) between any two time series for 
+    similarity measure. SBD is used for DBSCAN for clustering.
+    The main idea of DBSCAN is to find some cores in dense regions,and then 
+    expand the cores by transitivity of similarity to form clusters.
     
+    Args:
+        List(np.ndarray): a list consists of all time series(np.ndarray),the 
+            lengths of different time series could be different
+        minPts(np.int32): The core `p` in DBSCAN is defined as an object that has
+            at least `minPts` objects within a distance of ϵ from it(excluding
+            `p`). The default value of `minPts` is 4.
+    
+    Returns:
+        np.ndarray: for each time series, take the SBD between it and its 
+        minPts-Nearest-Neighbor(KNN). The SBDs of all time series in `values`
+        returned as an np.ndarray.
+    """
+    
+    if len(values_list) < minPts:
+        raise ValueError ('`values_list` must contain more than %d time series'\
+                          %minPts)
+    if len(values_list[0].shape) != 1:
+        raise ValueError ('`values` must be a 1-D array')
+    if (type(minPts) is not int) or (minPts < 1):
+        raise ValueError ('`minPts` must be a positive integar')
+    
+    #Caculate the SBD between any two time time series
+    sbd_matrix = np.zeros((len(values_list),len(values_list)))
+    for i in range(len(values_list)):
+        for j in range(i,len(values_list)):
+            sbd_matrix[i][j] = sbd_ele(values_list[i],values_list[j])
+            sbd_matrix[j][i] = sbd_matrix[i][j]
+    
+    #Return the minPts nearest SBD for each time series(excluding itself)
+    ret_sbd = np.zeros(len(values_list))
+    for i in range(len(values_list)):
+        src_index = np.argsort(sbd_matrix[i])
+        ret_sbd[i] = sbd_matrix[i][src_index][minPts]
+    
+    return sbd_matrix,ret_sbd
